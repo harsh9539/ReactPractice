0a9654386d6aef7830e2510cb716d43488d5cf75
harsh9539 <goyalh862@gmail.com>
2022-11-01 19:09:22 +0530

    Leaning useContext

--- src/Components/Login/LoginContext.js
+++ src/Components/Login/LoginContext.js
@@ -7,8 +7,7 @@ export const LoginContext = createContext();
 const LoginProvider = ({ children }) => {
     const [login, setLogin] = useState('');
     return (
-        <LoginContext.Provider value={{login,setLogin
-        }}>
+        <LoginContext.Provider value={{login,setLogin}}>
                 {children}
         </LoginContext.Provider>
     )
